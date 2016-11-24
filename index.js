496adcf2cac0ddc842e389e4121f0356adbf9f3a
Anil Bisalehalli Prasannakumar <anil.kumar@ringcentral.com>
2016-11-23 18:56:58 -0800

    Setting up subscriptions on all extensions

--- index.js
+++ index.js
@@ -1,88 +1,274 @@
-#! /usr/bin/env node
+'use strict';
 
-
-(function() {
-
-
-// Require RCSDK
-var RCSDK = require('rcsdk');
-var fs = require('fs');
-var extensions = [];
-var rcsdk = new RCSDK({
-	server: 'https://platform.devtest.ringcentral.com', // https://platform.ringcentral.com ( Production )
-	appKey: 'enter app Key',
-	appSecret: 'enter app Secret'
-});
-
-// constructor
-var RingCentral = module.exports = function RingCentral(options) {
-    options = options || {};
-
-    // Instance of RC Platform Singleton
-    this.platform = rcsdk.getPlatform();
-    this.log = rcsdk.getLog();
-
-    // RC Auth
-    this.auth = null;
-
-    return this;
+// Handle local development and testing
+if( process.env.RC_ENVIRONMENT !== 'Production' ) {
+    require('dotenv').config();
 }
 
-//Platform Singleton
-var platform = rcsdk.getPlatform();
+// CONSTANTS - obtained from environment variables
+var PORT = process.env.PORT;
 
 
-// Authenticate
-platform.authorize({
-	username: 'username',            // Enter the usernmae
-	extension: 'extension',          // Enter the extension
-	password: 'password',            // Enter the password
-	remember: 'true'
-}).then(function(response){
-  
-    platform.auth = response.json;
-    console.log('Authentication success');
-    console.log(JSON.stringify(response.data, null, 2));
+// Dependencies
+var RC = require('ringcentral');
+var helpers = require('ringcentral-helpers');
+var http = require('http');
 
-  // ********** Retreive Extensions ********************
-    // retreive all the extensions for this account
-   platform.get('/account/~/extension/')
-            .then(function(response){
-              
-              var apiresponse = response.json;
-              for(var key in apiresponse.records) {
-                var extension_number = "";
 
-                if (apiresponse.records[key].hasOwnProperty('extensionNumber')) {
-                    extension_number = parseInt(apiresponse.records[key].id);
-                    extensions.push(['/account/~/extension/' + extension_number + '/presence']);
+
+// VARS
+var _devices=[];
+var _extensionFilterArray = [];
+var server = http.createServer();
+
+
+
+    // Initialize the sdk for RC
+    var sdk = new RC({
+        server: process.env.RC_API_BASE_URL,
+        appKey: process.env.RC_APP_KEY,
+        appSecret: process.env.RC_APP_SECRET,
+        cachePrefix: process.env.RC_CACHE_PREFIX
+    });
+
+
+    // Bootstrap Platform and Subscription
+    var platform = sdk.platform();
+    var subscription = sdk.createSubscription();
+
+    return platform.login({
+        username: process.env.RC_USERNAME,
+        password: process.env.RC_PASSWORD,
+        extension: process.env.RC_EXTENSION,
+        cachePrefix: process.env.RC_CACHE_PREFIX
+    })
+        .then(function (response) {
+            console.log("The RC auth object is :", JSON.stringify(response.json(), null, 2));
+            console.log("Succesfully logged into the RC Account");
+            init();
+        })
+        .catch(function (e) {
+            console.log("Login Error into the Ringcentral Platform :", e);
+            throw e;
+        });
+
+
+
+// Start the server
+server.listen(PORT);
+
+/*
+ Retreive devices on Login success
+ */
+function init(loginData) {
+
+    var devices = [];
+    var page = 1;
+
+    function getDevicesPage() {
+
+        return platform
+            .get('/account/~/extension', {
+                page: page,
+                perPage: process.env.DEVICES_PER_PAGE                                             //REDUCE NUMBER TO SPEED BOOTSTRAPPING
+            })
+            .then(function (response) {
+
+                console.log("The account level extensions are :", JSON.stringify(response.json(), null, 2));
+                var data = response.json();
+
+                console.log("************** THE NUMBER OF ACCOUNT LEVEL Extensions ARE : ***************",data.records.length);
+
+                devices = devices.concat(data.records);
+                if (data.navigation.nextPage) {
+                    page++;
+                    return getDevicesPage();                                                     // this will be chained
+                } else {
+                    return devices;                                                              // this is the finally resolved thing
                 }
-            }
-            
-            // Create a subscription
-                var subscription = rcsdk.getSubscription();
-             
-                subscription.setEvents(extensions);
+            });
 
-                subscription.on(subscription.events.notification, function(msg) {
-                console.log(JSON.stringify(msg));
-                });
-             
-                return subscription.register();
+    }
 
-          })
-          .then(function(response) {
-                console.log('Subscription success');
-                console.log(JSON.stringify(response.data, null, 2));;
-          })
-          .catch(function(e){
-            console.error('Error ' + e.stack);
-          });
+    /*
+     Loop until you capture all devices
+     */
+    return getDevicesPage()
+        .then(function (devices) {
+            console.log("************** The total extensions are : **********", devices.length);
+            return devices;
+        })
+        .then(createEventFilter)
+        .then(startSubscription)
+        .catch(function (e) {
+            console.error(e);
+            throw e;
+        });
 
-   })
-   .catch(function(e){
-    console.error('Error ' + e.stack);
-  });
-   
+}
 
-  }) ();       
+/*
+ To generate the presence Event Filter for subscription
+ */
+function createEventFilter(devices) {
+    _devices = devices;
+    for (var i = 0; i < devices.length; i++) {
+
+        var device = devices[i];
+        _extensionFilterArray.push(generatePresenceEventFilter(device));
+    }
+    return devices;
+}
+
+function generatePresenceEventFilter(item) {
+    //console.log("The item is :", item);
+    if (!item) {
+        ;
+        throw new Error('Message-Dispatcher Error: generatePresenceEventFilter requires a parameter');
+    } else {
+        console.log("The Presence Filter added for the extension :" + item.id + ' : /account/~/extension/' + item.id + '/presence?detailedTelephonyState=true');
+        return '/account/~/extension/' + item.id + '/presence?detailedTelephonyState=true';
+    }
+}
+
+
+function startSubscription(devices) { //FIXME MAJOR Use devices list somehow
+
+    console.log("********* STARTING TO CREATE SUBSCRIPTION ON ALL FILTERED DEVICES ***************");
+    return subscription
+        .setEventFilters(_extensionFilterArray)
+        .register();
+}
+
+
+/**
+ * Application Functions
+ **/
+//TODO: MAJOR Refactor to handle multiple channels for notification (such as webhooks, etc...)
+function sendAlerts(response) {
+
+    // Send alerts to each of the SMS in the array as defined in environment variable `ALERT_SMS`
+    console.log("ALERT SMS SENT TO NUMBERS :", ALERT_SMS);
+    return Promise.all(ALERT_SMS.map(function (ext) {
+        console.log("Inside Send Alert Success");
+        return sendSms(ext);
+    })).catch(function (e) {
+        console.log("The error is with the promises", e);
+        throw e;
+    });
+
+}
+
+function getAllAccounts(){
+
+    var accountArray = [{"username":"18887601940","password":"Deeson525422"},{"username":"12024172122","password":"Five5FIVE"},{"username":"14158774683","password":"R1ngcentral"},{"username":"16505626210","password":"16505626210"}];
+    // Send alerts to each of the SMS in the array as defined in environment variable `ALERT_SMS`
+    console.log("Starting to load the accounts :", accountArray);
+
+    return Promise.all(accountArray.map(function(arr){
+        console.log("The account which is now going is : ", arr);
+        console.log("The username is : ", arr);
+
+
+    }))
+
+}
+
+
+// Server Event Listeners
+server.on('request', inboundRequest);
+
+server.on('error', function (err) {
+    console.error(err);
+});
+
+server.on('listening', function () {
+    console.log('Server is listening to ', PORT);
+});
+
+server.on('close', function () {
+    console.log('Server has closed and is no longer accepting connections');
+});
+
+// Register Platform Event Listeners
+platform.on(platform.events.loginSuccess, handleLoginSuccess);
+platform.on(platform.events.loginError, handleLoginError);
+platform.on(platform.events.logoutSuccess, handleLogoutSuccess);
+platform.on(platform.events.logoutError, handleLogoutError);
+platform.on(platform.events.refreshSuccess, handleRefreshSuccess);
+platform.on(platform.events.refreshError, handleRefreshError);
+
+// Register Subscription Event Listeners
+subscription.on(subscription.events.notification, handleSubscriptionNotification);
+subscription.on(subscription.events.removeSuccess, handleRemoveSubscriptionSuccess);
+subscription.on(subscription.events.removeError, handleRemoveSubscriptionError);
+subscription.on(subscription.events.renewSuccess, handleSubscriptionRenewSuccess);
+subscription.on(subscription.events.renewError, handleSubscriptionRenewError);
+subscription.on(subscription.events.subscribeSuccess, handleSubscribeSuccess);
+subscription.on(subscription.events.subscribeError, handleSubscribeError);
+
+// Server Request Handler
+function inboundRequest(req, res) {
+    //console.log('REQUEST: ', req);
+}
+
+/**
+ * Subscription Event Handlers   - to capture events on telephonyStatus ~ callConnected
+ **/
+function handleSubscriptionNotification(msg) {
+    console.log('*************** SUBSCRIPTION NOTIFICATION: ****************(', JSON.stringify(msg, null, 2));
+}
+
+function handleRemoveSubscriptionSuccess(data) {
+    console.log('REMOVE SUBSCRIPTION SUCCESS DATA: ', data);
+}
+
+function handleRemoveSubscriptionError(data) {
+    console.log('REMOVE SUBSCRIPTION ERROR DATA: ', data);
+}
+
+function handleSubscriptionRenewSuccess(data) {
+    console.log('RENEW SUBSCRIPTION SUCCESS DATA: ', data);
+}
+
+function handleSubscriptionRenewError(data) {
+    console.log('RENEW SUBSCRIPTION ERROR DATA: ', data);
+}
+
+function handleSubscribeSuccess(data) {
+    console.log('SUBSCRIPTION CREATED SUCCESSFULLY');
+}
+
+function handleSubscribeError(data) {
+    console.log('FAILED TO CREATE SUBSCRIPTION: ', data);
+}
+
+/**
+ * Platform Event Handlers
+ **/
+function handleLoginSuccess(data) {
+    // UNCOMMENT TO VIEW LOGIN DATA
+    //console.log('LOGIN SUCCESS DATA: ', data);
+}
+
+function handleLoginError(data) {
+    console.log('LOGIN FAILURE DATA: ', data);
+}
+
+function handleLogoutSuccess(data) {
+    console.log('LOGOUT SUCCESS DATA: ', data);
+}
+
+function handleLogoutError(data) {
+    console.log('LOGOUT FAILURE DATA: ', data);
+}
+
+function handleRefreshSuccess(data) {
+    console.log('REFRESH SUCCESS DATA: ', data);
+}
+
+function handleRefreshError(data) {
+    console.log('REFRESH FAILURE DATA: ', data);
+    console.log('Initialing Login again :');
+    login();
+}
